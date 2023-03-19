2a8014689eb8b70a69fdd9cffe9b13326adc7826
GashonHussein <gashon96@gmail.com>
2023-03-18 18:39:09 -0700

    fix: flag for hyperparam tuning

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -330,7 +330,7 @@ def get_args():
     parser.add_argument("--hidden_dropout_prob", type=float, default=0.3)
     parser.add_argument("--lr", type=float, help="learning rate, default lr for 'pretrain': 1e-3, 'finetune': 1e-5",
                         default=1e-5)
-    parser.add_argument("--tune_hyperparams", type=bool, default=False)
+    parser.add_argument("--tune_hyperparams")
 
     args = parser.parse_args()
     return args
